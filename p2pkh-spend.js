e9c10f9c0fd4e2cc8295e173cf77fa6683c7a6e5
Chris Troutner <chris.troutner@gmail.com>
2023-01-30 15:52:57 -0800

    Added all background info

---
+++ p2pkh-spend.js
@@ -0,0 +1,9 @@
+/*
+  This simple example recreates the most common type of transaction, a
+  pay-to-pubkey-hash (P2PKH) transaction. The purpose of this example script
+  is to illustrate how libauth and the template files work.
+*/
+
+// import { p2pkhTemplate, localCryptosTemplate } from './templates.js'
+
+console.log('hello world')
